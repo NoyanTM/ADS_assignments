e268c5e9a0eb7e8f39773a7e354d957041a55e91
NoyanTM <doge.channel@gmail.com>
2022-05-01 20:25:42 +0600

    MyHashTable with comments

--- ADS_assignment3/src/com/company/Main.java
+++ ADS_assignment3/src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
     public static void main(String[] args){
         MyHashTable<String, Integer> HashTable = new MyHashTable<>();
-        HashTable.add("ADS", 4);
-        HashTable.add("ADS", 4);
         HashTable.add("JAVA", 1);
-        HashTable.add("JAVA", 3);
         HashTable.add("OOP", 2);
+        HashTable.add("JAVA", 3);
+        HashTable.add("JAVA", 4);
+        HashTable.add("ADS", 4);
 
         System.out.println(HashTable.size());
         System.out.println(HashTable.isEmpty());
--- ADS_assignment3/src/com/company/MyHashTable.java
+++ ADS_assignment3/src/com/company/MyHashTable.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class MyHashTable <K,V> {
-    private ArrayList<HashNode<K,V>> bucketArray;
-    private int numBuckets;
+    private ArrayList<HashNode<K,V>> bucketArray; // array with chain of nodes; K - key, V - value;
+    private int numBuckets; // capacity of array
 
-    private int size;
+    private int size; // actual number or size of elements
 
     public MyHashTable(){
         bucketArray = new ArrayList<>();
@@ -14,14 +14,16 @@ public class MyHashTable <K,V> {
         size = 0;
 
         for (int i = 0; i < numBuckets; i++){
-            bucketArray.add(null);
+            bucketArray.add(null); // filling empty array with null elements
         }
     }
 
+    // size() - get size
     public int size(){
         return size;
     }
 
+    // isEmpty() - checking empty or not -> return true or false
     public boolean isEmpty(){
         return size() == 0;
     }
@@ -34,17 +36,18 @@ public class MyHashTable <K,V> {
         int hashCode = hashCode(key);
         int index = hashCode % numBuckets;
 
-        index = index<0?index*-1:index;
+        index = (index < 0) ? index * (-1) : index; // checking index - if index negative, multiplying by (-1)
         return index;
     }
 
+    // remove() - remove element
     public V remove (K key){
         int bucketIndex = getBucketIndex(key);
         int hashCode = hashCode(key);
 
         HashNode<K,V> head = bucketArray.get(bucketIndex);
 
-        HashNode<K,V> prev = null;
+        HashNode<K,V> prev = null; // extra node for connecting prev node with current node
         while (head != null){
             if (head.key.equals(key) && hashCode == head.hashCode){
                 break;
@@ -60,27 +63,29 @@ public class MyHashTable <K,V> {
 
         size--;
 
-        if (prev != null) {prev.next = head.next;}
-        else {bucketArray.set(bucketIndex, head.next);}
+        if (prev != null) {prev.next = head.next;} // if element is located in the middle
+        else {bucketArray.set(bucketIndex, head.next);} // if element is located at the beginning
         return head.value;
     }
 
+    // get() - getting value of element by using key.
     public V get(K key){
         int bucketIndex = getBucketIndex(key);
         int hashCode = hashCode(key);
 
-        HashNode<K,V> head = bucketArray.get(bucketIndex);
+        HashNode<K,V> head = bucketArray.get(bucketIndex); // starting from the head and getting pointer to the first element inside bucket
 
         while (head != null){
-            if (head.key.equals(key) && head.hashCode == hashCode){
-                return head.value;
-            }
+            if (head.key.equals(key) && head.hashCode == hashCode) // checking key with hashcode, because keys can be same with other element
+            {return head.value;}
+
             head = head.next;
         }
 
         return null;
     }
 
+    // add() - adding elements
     public void add(K key, V value){
         int bucketIndex = getBucketIndex(key);
         int hashCode = hashCode(key);
@@ -100,6 +105,7 @@ public class MyHashTable <K,V> {
         newNode.next = head;
         bucketArray.set(bucketIndex, newNode);
 
+        // increasing capacity of array, if 70% of array filled
         if ((1.0 * size) / numBuckets >= 0.7){
             ArrayList<HashNode<K,V>> temp = bucketArray;
             bucketArray = new ArrayList<>();
